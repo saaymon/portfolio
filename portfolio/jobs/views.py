64fd6929389f5d18d2e31956e8d638547842eb9c
saaymon <37156005+saaymon@users.noreply.github.com>
2019-02-12 00:19:51 +0100

    many changes

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -1,8 +1,30 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
 from .models import Job
-
+from blog.models import Blog
 # Create your views here.
-def home(request):
+
+class Allview(TemplateView):
+
+    template_name = 'jobs/home.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(Allview, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['jobs'] = Job.objects.all().order_by('-id')[:3]
+        context['blogs'] = Blog.objects.all()
+        return context
+
+
+def about(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    return render(request, 'jobs/aboutme.html', {'jobs':jobs})
+
+
+
+
+
+
+
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,12 +1,16 @@
+from django.views.generic.list import ListView
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from jobs.views import Allview
 import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home'),
-    path('blog/', include('blog.urls'))
+    path('', Allview.as_view(), name='home'),
+    path('about/', jobs.views.about, name='about'),
+    path('blog/', include('blog.urls')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
