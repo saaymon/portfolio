f45e72d25f58a6a63970c1e90622226b48f47a4a
saaymon <37156005+saaymon@users.noreply.github.com>
2019-06-09 22:42:47 +0200

    db changed to local

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': 'redhat',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'sqlite3.db',
 
     }
 }
